300e43e1227286019eb08fd97f678373f38dfa89
Rujipas Varathikul <deadbook60@gmail.com>
2025-10-21 14:39:26 +0700

    Add lecture 2 - first part

--- day-2.py
+++ day-2.py
@@ -1,1 +1,42 @@
+# comment
+# This is also a comment
+# This part of the code is to
+# gather user information for login
+
+'''
+This is a very long
+and very helpful comment
+Believe Me!
+'''
+
+# print function output the result in the terminal area
+# function_name(input_1, input_2, input_3)
+# -> argument = input of function
+print('Goodbye')
 print('Hello, World!')
+print('This is the third line.')
+print(1, 2, 6, 'Hello')
+print(1, 2, 6, 'Hello', sep='-')
+print(1, 2, 6, 'Hello', sep='-t-')
+
+# Data Types
+# number of student, name, the money, yes or no
+
+# number
+# integer
+# 3, 5, -4, 201
+# floating point number
+# 3.5, 0.64, -2.76, 5.00
+
+# text -> string
+# 'Hello', "The End", "5.00"
+
+# Boolean (truth value)
+# True, False
+
+# NoneType
+# None
+
+print(type(56))
+print(type(-2.0))
+print(type('-2.0'))
